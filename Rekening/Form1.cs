5eb7313ed31d3cc3e5081bd9ae61423b7f3d6461
MrBlubke <ward@impesoft.com>
2021-01-20 17:23:46 +0100

    remove english account replace by dutch rekening

--- Rekening/Form1.cs
+++ Rekening/Form1.cs
@@ -12,7 +12,7 @@ namespace Rekening
 {
     public partial class Form1 : Form
     {
-        Rekening account = new Rekening("123-4530396-13", 100);
+        Rekening rekening = new Rekening("123-4530396-13", 100);
         SpaarRekening spaar = new SpaarRekening("735-1420224-12", 200, 5);
         ZichtRekening zicht = new ZichtRekening("987-6543210-12", 25);
         public Form1()
@@ -22,14 +22,14 @@ namespace Rekening
        
         private void RekeningPlus_Click(object sender, EventArgs e)
         {
-            account.Storten(Convert.ToDouble(invoerRekening.Text));
-            uitvoerRekening.Text = account.ToString();
+            rekening.Storten(Convert.ToDouble(invoerRekening.Text));
+            uitvoerRekening.Text = rekening.ToString();
         }
 
         private void RekeningMin_Click(object sender, EventArgs e)
         {
-            account.Afhalen(Convert.ToDouble(invoerRekening.Text));
-            uitvoerRekening.Text = account.ToString();
+            rekening.Afhalen(Convert.ToDouble(invoerRekening.Text));
+            uitvoerRekening.Text = rekening.ToString();
         }
 
         private void SpaarPlus_Click(object sender, EventArgs e)
@@ -64,7 +64,7 @@ namespace Rekening
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            uitvoerRekening.Text = account.ToString();
+            uitvoerRekening.Text = rekening.ToString();
             uitvoerSpaar.Text = spaar.ToString();
             uitvoerZicht.Text = zicht.ToString();
         }
